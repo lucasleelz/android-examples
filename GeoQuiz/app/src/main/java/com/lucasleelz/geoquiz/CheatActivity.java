b49a297f4df411a67156d616c54ebcaf3b41c644
lucas lee <llzqingdao2012@gmail.com>
2017-08-05 22:32:07 +0800

    CheatActivity屏幕旋转，防止数据丢失。

--- GeoQuiz/app/src/main/java/com/lucasleelz/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/lucasleelz/geoquiz/CheatActivity.java
@@ -11,10 +11,12 @@ public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.lucasleelz.geoquiz.CheatActivity.answer_id_true";
     private static final String EXTRA_ANSWER_SHOWN = "com.lucasleelz.geoquiz.CheatActivity.answer_shown";
+    private static final String ANSWER_SHOWN = "answer_shown";
 
     private TextView mAnswerTextView;
 
     private Button mShowAnswerButton;
+    private boolean mAnswerShow;
 
     public static Intent createIntent(Context packageContext, boolean answerIsTrue) {
         Intent result = new Intent(packageContext, CheatActivity.class);
@@ -31,20 +33,41 @@ public class CheatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
-        boolean answerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
-
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
         mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
+
+        boolean answerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        if (savedInstanceState != null) {
+            mAnswerShow = savedInstanceState.getBoolean(ANSWER_SHOWN, false);
+            updateAnswerTextView(answerIsTrue);
+            setAnswerShownResult();
+        }
+
         mShowAnswerButton.setOnClickListener(view -> {
-            int AnswerResId = answerIsTrue ? R.string.true_button : R.string.false_button;
-            mAnswerTextView.setText(AnswerResId);
-            setAnswerShownResult(true);
+            mAnswerShow = true;
+            updateAnswerTextView(answerIsTrue);
+            setAnswerShownResult();
         });
     }
 
-    private void setAnswerShownResult(boolean isAnswerShown) {
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putBoolean(ANSWER_SHOWN, mAnswerShow);
+    }
+
+    private void setAnswerShownResult() {
         Intent data = new Intent();
-        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        data.putExtra(EXTRA_ANSWER_SHOWN, mAnswerShow);
         setResult(RESULT_OK, data);
+    }
+
+    private void updateAnswerTextView(boolean answerIsTrue) {
+        if (!mAnswerShow) {
+            return;
+        }
+        int AnswerResId = answerIsTrue ? R.string.true_button : R.string.false_button;
+        mAnswerTextView.setText(AnswerResId);
     }
 }
