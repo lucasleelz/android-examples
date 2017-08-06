8defc5eda5cb54d83df699a77689c5c1900a8b13
lucas lee <llzqingdao2012@gmail.com>
2017-08-06 23:15:00 +0800

    显示日期对话框。

--- CriminalIntent/app/src/main/java/com/lucaslz/criminalintent/DatePickerFragment.java
+++ CriminalIntent/app/src/main/java/com/lucaslz/criminalintent/DatePickerFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by lucas lee <llzqingdao2012gmail.com> on 2017/8/6.
@@ -14,7 +16,11 @@ public class DatePickerFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        View datePicker = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(datePicker)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
