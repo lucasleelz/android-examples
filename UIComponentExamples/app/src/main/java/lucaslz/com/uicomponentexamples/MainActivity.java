109e14029bf8db55157b1988e3bdcb838531fbb6
lucas lee <llzqingdao2012@gmail.com>
2017-10-07 22:27:55 +0800

    初始化UI组件例子。

---
+++ UIComponentExamples/app/src/main/java/lucaslz/com/uicomponentexamples/MainActivity.java
@@ -0,0 +1,13 @@
+package lucaslz.com.uicomponentexamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
