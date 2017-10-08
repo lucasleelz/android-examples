16ff8f5f73209e49ec3355eb903d2f8a8667a91d
lucas lee <llzqingdao2012@gmail.com>
2017-10-08 17:51:48 +0800

    LayoutActivity显示标题以及返回按钮。

--- UIComponentExamples/app/src/main/java/lucaslz/com/uicomponentexamples/LayoutActivity.java
+++ UIComponentExamples/app/src/main/java/lucaslz/com/uicomponentexamples/LayoutActivity.java
@@ -2,7 +2,11 @@ package lucaslz.com.uicomponentexamples;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
 
 import lucaslz.com.component.activity.SingleFragmentActivity;
 
@@ -18,5 +22,26 @@ public class LayoutActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return LayoutFragment.newInstance();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar == null) {
+            return;
+        }
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
