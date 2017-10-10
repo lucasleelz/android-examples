4d8167d6b56d1e4b5977cdc60bde7f5c4445cc5a
lucas lee <llzqingdao2012@gmail.com>
2017-10-10 11:11:58 +0800

    自定义toolbar。防止在DrawerLayout时header还存在。

--- UIComponentExamples/app/src/main/java/com/lucaslz/uicomponentexamples/MainActivity.java
+++ UIComponentExamples/app/src/main/java/com/lucaslz/uicomponentexamples/MainActivity.java
@@ -4,34 +4,36 @@ import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.lucaslz.component.activity.BaseActivity;
 
 public class MainActivity extends BaseActivity {
 
+    private Toolbar mToolbar;
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = (MenuItem item) -> {
         FragmentManager fragmentManager = getSupportFragmentManager();
-//        ActionBar actionBar = getSupportActionBar();
+        ActionBar actionBar = getSupportActionBar();
         switch (item.getItemId()) {
             case R.id.navigation_home:
                 fragmentManager.beginTransaction()
                         .replace(R.id.navigation_fragment_container, HomeFragment.newInstance())
                         .commit();
-//                actionBar.setTitle("Home");
+                actionBar.setTitle("Home");
                 return true;
             case R.id.navigation_dashboard:
                 fragmentManager.beginTransaction()
                         .replace(R.id.navigation_fragment_container, HomeFragment.newInstance())
                         .commit();
-//                actionBar.setTitle("Dashboard");
+                actionBar.setTitle("Dashboard");
                 return true;
             case R.id.navigation_profile:
                 fragmentManager.beginTransaction()
                         .replace(R.id.navigation_fragment_container, ProfileFragment.newInstance())
                         .commit();
-//                actionBar.setTitle("Profile");
+                actionBar.setTitle("Profile");
                 return true;
         }
         return false;
@@ -42,15 +44,15 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setTitle("Home");
+        setSupportActionBar(mToolbar);
+
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
                 .add(R.id.navigation_fragment_container, HomeFragment.newInstance())
                 .commit();
-
-//        ActionBar actionBar = getSupportActionBar();
-//        actionBar.setTitle("Home");
     }
-
 }
